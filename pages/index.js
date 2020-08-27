ffb5839554727d5546f347ca488b34d5ddaf0175
김홍규 <khkhong1@gmail.com>
2020-08-27 12:54:58 +0900

    [#7] timer 코드 추가 추후에 API 열리면 주석 해제 및 불필요 코드 제거 추가 작업 필요.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import React, { useCallback, useEffect } from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import Router from 'next/router';
-import { COMMON_MESSAGE } from '../src/const/commonMessage';
+import {COMMON_MESSAGE} from '../src/const/commonMessage';
 import dynamic from "next/dynamic";
 import Util from "../src/util/Util";
 import dummy from "../src/dummy/dummy";
@@ -8,19 +8,45 @@ import {useDispatch, useSelector} from "react-redux";
 import {GIMP_ACTION} from "../src/reducer/gimp";
 
 /** Hammer.js, zoom plugin이 window 객체를 사용하기 때문에 SSR 하면 안됨. */
-const ChartComponent = dynamic(() => import('../src/components/chart-component'), { ssr: false});
+const ChartComponent = dynamic(() => import('../src/components/chart-component'), {ssr: false});
 
 const Home = (props) => {
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const { gimpMarginsData } = useSelector((state) => state.gimp);
+  const {gimpMarginsData} = useSelector((state) => state.gimp);
 
-    useEffect(() => {
-      // dispatch({
-      //   type: GIMP_ACTION.GIMP_MARGINS_REQUEST
-      // });
-    }, []);
+  const [timer, setTimer] = useState(new Date());
+
+  useEffect(() => {
+
+  }, []);
+
+  /**
+   * @todo 추후에 API 열리면 해당 코드 주석 해제 후 timer, renderTimer 코드 제거
+   * @author kimhg
+   * */
+  useEffect(() => {
+
+    const timerID = setInterval(
+      () => {
+        setTimer(new Date());
+        // dispatch({
+        //   type: GIMP_ACTION.GIMP_MARGINS_REQUEST
+        // });
+      }, 60000);
+
+    return () => {
+      clearInterval(timerID);
+    };
+  }, [timer]);
+
+  const renderTimer = useCallback(() => {
+    return (
+      <div>{timer.getTime()}</div>
+    );
+  }, [timer]);
+
 
   // const onClickButton = useCallback((e) => {
   //   e.preventDefault();
@@ -32,15 +58,16 @@ const Home = (props) => {
       {COMMON_MESSAGE.INDEX_WELCOME_STRING}
       <br/>
       <div>
-      <ChartComponent
-        chartData={Util.convertGimpDataToChartData(dummy)}
-        // chartData={Util.convertGimpDataToChartData(gimpMarginsData)}
-        type={'line'}
-        height={200}
-      />
+        <ChartComponent
+          chartData={Util.convertGimpDataToChartData(dummy)}
+          // chartData={Util.convertGimpDataToChartData(gimpMarginsData)}
+          type={'line'}
+          height={200}
+        />
 
       </div>
-            {/*<button onClick={onClickButton}>To inputRouter</button>*/}
+      {renderTimer()}
+      {/*<button onClick={onClickButton}>To inputRouter</button>*/}
     </>
   );
 };
